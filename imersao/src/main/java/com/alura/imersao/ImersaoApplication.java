d2343a7badb9d4fab2818485dfbcf26249f164c6
DenerThiago21 <denerthi.ibi@gmail.com/>
2022-07-21 20:34:56 -0300

    first commit

---
+++ imersao/src/main/java/com/alura/imersao/ImersaoApplication.java
@@ -0,0 +1,13 @@
+package com.alura.imersao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImersaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImersaoApplication.class, args);
+	}
+
+}
